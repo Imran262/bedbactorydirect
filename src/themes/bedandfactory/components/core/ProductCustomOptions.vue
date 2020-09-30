27663a82ebd06b6cfc118671de84422f5ced2f38
Syed Ali Meesum <alimeesum2056@gmail.com>
2020-09-30 16:13:18 +0500

    Swatches custom input added in colors

--- src/themes/bedandfactory/components/core/ProductCustomOptions.vue
+++ src/themes/bedandfactory/components/core/ProductCustomOptions.vue
@@ -6,47 +6,90 @@
     <!-- {{product.custom_options[0]}} -->
     <form class="custom-options">
       <div
-        v-for="(option,count) in product.custom_options"
-        :key="('customOption_' + option.option_id)"
+        v-for="(option, count) in product.custom_options"
+        :key="'customOption_' + option.option_id"
       >
         <!-- {{option}} -->
         <!-- {{(count>1 && color)}}
         {{(count<=1 && !color)}}-->
-        <div v-if="count>1 && color" class="custom-option mb15 basin">
+        <!-- For color only -->
+        <div v-if="count > 1 && color" class="custom-option mb15 basin">
           <h4 class="basin-head">{{ option.title }}</h4>
+          <!-- {{option}} -->
           <!-- about to call function {{ setOptions()}} -->
-          <!-- First conditoin is true
-          {{count }}
-          {{color }}-->
-          <!-- <button type="button" @click="showColorPicker">{{ $t('Select color')}}</button>
-          <div id="overlay" @click="hideColorPicker" v-if="hamza" />
-          <color-picker :colors="option" v-if="hamza" />-->
           <div
             class="mt5 mb5 relative basin_size"
             v-if="option.type === 'select' || option.type === 'drop_down'"
           >
             <select
+              ref="colorImage"
               :name="'customOption_' + option.option_id"
               class="m0 no-outline"
               v-model="inputValues['customOption_' + option.option_id]"
               @focus="$emit('focus')"
               @blur="$emit('blur')"
-              @change="optionChanged(option)"
+              @change="
+                optionChanged(option);
+                setImage();
+              "
             >
-              <!-- <option
-              v-for="(opval, key) in option.values"
-              :key="key"
-              :value="opval.option_type_id"
-              >{{ opval.title }}</option>-->
-              <option :value="null" :key="2378695843" selected>Please select</option>
+              <option disabled value="" :key="2378695843" selected="selected">
+                Please select
+              </option>
               <template v-for="(opval, key) in option.values">
-                <option v-if="key==0" :value="opval.option_type_id" :key="key">{{ opval.title }}</option>
-                <option v-else :value="opval.option_type_id" :key="key">{{ opval.title }}</option>
+                <option
+                  v-if="key == 0"
+                  :value="opval.option_type_id"
+                  :key="key"
+                >
+                  {{ opval.title }}
+                </option>
+                <option v-else :value="opval.option_type_id" :key="key">
+                  {{ opval.title }}
+                </option>
               </template>
             </select>
+            <div
+              v-for="(value, vIndex) in option.values"
+              :key="vIndex + value.option_type_id"
+            >
+              <div v-if="value.option_type_id == imageSrc">
+                <img
+                  v-if="
+                    !(
+                      value.layer == '' ||
+                      value.layer == null ||
+                      value.layer == ' '
+                    )
+                  "
+                  class="selctedImage"
+                  :src="
+                    'http://m2.bedfactorydirect.co.uk/pub/media/catalog/layer' +
+                    value.layer
+                  "
+                  alt="Image"
+                />
+              </div>
+            </div>
+            <div
+              v-for="(value, vIndex) in option.values"
+              :key="vIndex + value.layer"
+            >
+              <img
+                class="allImages"
+                :src="
+                  'http://m2.bedfactorydirect.co.uk/pub/media/catalog/layer' +
+                  value.layer
+                "
+                alt="Image"
+                @click="setOption(value, option)"
+              />
+            </div>
+            <div></div>
           </div>
         </div>
-        <div v-if="count<=1 && !color " class="custom-option mb15 basin">
+        <!-- For all other options -->
+        <div v-if="count <= 1 && !color" class="custom-option mb15 basin">
           <!-- second condition is true
           {{ count }}
           {{ color }}-->
@@ -55,9 +98,9 @@
             v-if="option.type === 'field'"
             class="py10 w-100 border-box brdr-none brdr-bottom-1 brdr-cl-primary h4 sans-serif"
             type="text"
-            :name="('customOption_' + option.option_id)"
+            :name="'customOption_' + option.option_id"
             focus
-            v-model="inputValues[('customOption_' + option.option_id)]"
+            v-model="inputValues['customOption_' + option.option_id]"
             :placeholder="option.title"
             @change="optionChanged(option)"
           />
@@ -72,15 +115,15 @@
               @change="optionChanged(option)"
               type="radio"
               class="m0 no-outline"
-              :name="('customOption_' + option.option_id)"
-              :id="('customOption_' + opval.option_type_id)"
+              :name="'customOption_' + option.option_id"
+              :id="'customOption_' + opval.option_type_id"
               focus
               :value="opval.option_type_id"
-              v-model="inputValues[('customOption_' + option.option_id)]"
+              v-model="inputValues['customOption_' + option.option_id]"
             />
             <label
               class="pl10 lh20 h4 pointer"
-              :for="('customOption_' + opval.option_type_id)"
+              :for="'customOption_' + opval.option_type_id"
               v-html="opval.title"
             />
           </div>
@@ -90,27 +133,36 @@
           >
             <!-- drop_down hello -->
             <select
-            ref="dropDown"
+              ref="dropDown"
               :name="'customOption_' + option.option_id"
               class="m0 no-outline"
               v-model="inputValues['customOption_' + option.option_id]"
               @focus="$emit('focus')"
               @blur="$emit('blur')"
-              @change="optionChanged(option);"
+              @change="optionChanged(option)"
             >
-           
               <!-- <option
               v-for="(opval, key) in option.values"
               :key="key"
               :value="opval.option_type_id"
               >{{ opval.title }}</option>-->
-              <option :value="null" :key="2378695843" selected>Please select</option>
+              <option disabled value="" :key="2378695843" selected="selected">
+                Please select
+              </option>
               <template v-for="(opval, key) in option.values">
-                <option v-if="key==0" :value="opval.option_type_id" :key="key">{{ opval.title }}</option>
-                <option v-else :value="opval.option_type_id" :key="key">{{ opval.title }}</option>
+                <option
+                  v-if="key == 0"
+                  :value="opval.option_type_id"
+                  :key="key"
+                >
+                  {{ opval.title }}
+                </option>
+                <option v-else :value="opval.option_type_id" :key="key">
+                  {{ opval.title }}
+                </option>
               </template>
             </select>
-             <!-- hello
+            <!-- hello
             {{option.option_id}} -->
             <!-- {{makeList()}} -->
           </div>
@@ -125,22 +177,25 @@
               @change="optionChanged(option)"
               type="checkbox"
               class="m0 no-outline"
-              :name="('customOption_' + option.option_id)"
-              :id="('customOption_' + opval.option_type_id)"
+              :name="'customOption_' + option.option_id"
+              :id="'customOption_' + opval.option_type_id"
               focus
               :value="opval.option_type_id"
-              v-model="inputValues[('customOption_' + option.option_id)]"
+              v-model="inputValues['customOption_' + option.option_id]"
             />
             <label
               class="pl10 lh20 h4 pointer"
-              :for="('customOption_' + opval.option_type_id)"
+              :for="'customOption_' + opval.option_type_id"
               v-html="opval.title"
             />
           </div>
           <span
             class="error"
-            v-if="validation.results[('customOption_' + option.option_id)].error"
-          >{{ validation.results[('customOption_' + option.option_id)].message }}</span>
+            v-if="validation.results['customOption_' + option.option_id].error"
+            >{{
+              validation.results["customOption_" + option.option_id].message
+            }}</span
+          >
         </div>
       </div>
     </form>
@@ -149,7 +204,7 @@
 
 <script>
 import { ProductCustomOptions } from "@vue-storefront/core/modules/catalog/components/ProductCustomOptions.ts";
-import { changeFilterQuery } from '@vue-storefront/core/modules/catalog-next/helpers/filterHelpers';
+import { changeFilterQuery } from "@vue-storefront/core/modules/catalog-next/helpers/filterHelpers";
 //import ColorPicker from "theme/components/core/blocks/ColorPIcker/ColorPicker";
 export default {
   mixins: [ProductCustomOptions],
@@ -158,22 +213,90 @@ export default {
   },
   data() {
     return {
-      options: []
+      options: [],
+      imageSrc: "",
     };
   },
   props: {
     color: {
       type: Boolean,
-      required: true
-    }
+      required: true,
+    },
   },
   methods: {
+  
+    setOption(value, option) {
+      // console.log(
+      //   "2244 in set option function",
+      //   value,
+      //   "\n selected option is ",
+      //   value.option_type_id,
+      //   "\n",
+      //   typeof this.inputValues,
+      //   "current color is ",
+      //   this.inputValues.customOption_15
+      // );
+      let data1 = this.$refs.colorImage;
+      // console.log(
+      //   "1155\n Selected value ",
+      //   value.option_type_id,
+      //   "\n before assignment",
+      //   data1
+      // );
+      this.inputValues.customOption_15 = value.option_type_id;
+      data1 = this.$refs.colorImage;
+      // console.log(
+      //   "\n Selected value ",
+      //   value.option_type_id,
+      //   "\n after assignment",
+      //   data1
+      // );
+      // this.setImage();
+      this.optionChanged(option);
+      this.imageSrc = value.option_type_id;
+      // setTimeout(()=>{
+      //   console.log('Arsltest', this.$refs.colorImage[0].selectedOptions[0])
+      //   let data = this.$refs.colorImage[0].selectedOptions[0].value;
+      //   console.log("\n after assignment", data);
+      //   this.imageSrc = value.option_type_id;
+      //   }, 100);
+    },
+    setImage() {
+      // console.log(
+      //   "1122 in set image function\n",
+      //   this.$refs.colorImage,
+      //   "\n\n\n"
+      // );
+      // console.log(
+      //   "1144 in set image function\n",
+      //   JSON.stringify(this.$refs.colorImage),
+      //   "\n\n\n"
+      // );
+      // console.log(
+      //   "1133 length\n",
+      //   typeof this.$refs.colorImage[0].selectedOptions,
+      //   "\n\n\n"
+      // );
+      let data = this.$refs.colorImage[0].selectedOptions[0].value;
+      this.imageSrc = data;
+      // console.log(
+      //   "1122 \n selected option is  ",
+      //   data,
+      //   "image source is ",
+      //   this.imageSrc
+      // );
+
+      // let input = this.$refs.colorImage[0].selectedOptions[0];
+      // // this.$refs.colorImage[0].selectedOptions[0].text = "red"
+      // console.log("1133 length\n", input, "\n\n\n");
+      // 0.selectedOption[0].label
+    },
     // makeList(option) {
     //   console.log("112233 options are ", this.options.length);
     //   console.log("Here we are");
-      
+
     //   console.log("refernce is ",this.$refs.dropDown);
-      
+
     //   if (this.options.length > 0) {
     //   }
     // },
@@ -190,10 +313,16 @@ export default {
     // sendSizeOption(option) {
     //   this.$emit("size", option);
     // }
-  }
+  },
 };
 </script>
 <style lang="scss" scoped>
+.allImages {
+  width: 100px;
+}
+.selctedImage {
+  width: 100px;
+}
 @import "~theme/css/variables/colors";
 @import "~theme/css/helpers/functions/color";
 $color-tertiary: color(tertiary);
