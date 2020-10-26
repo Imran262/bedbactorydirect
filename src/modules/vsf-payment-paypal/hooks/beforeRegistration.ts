c1ff14118a8df1d5537aa3efd217f6434012fac8
Syed Ali Meesum <alimeesum2056@gmail.com>
2020-10-26 12:52:36 +0500

    paypal module adding again

---
+++ src/modules/vsf-payment-paypal/hooks/beforeRegistration.ts
@@ -0,0 +1,16 @@
+import { isServer } from '@vue-storefront/core/helpers'
+import { currentStoreView } from '@vue-storefront/core/lib/multistore'
+
+export function beforeRegistration(config, store) {
+  const VSF_PAYPAL_CODE = 'paypal_express'
+
+  if (!isServer && config.hasOwnProperty('paypal')) {
+    const storeView = currentStoreView()
+    const { currencyCode } = storeView.i18n
+    const clientId = config.paypal.hasOwnProperty('clientId') ? config.paypal.clientId : ''
+    const sdkUrl = `https://www.paypal.com/sdk/js?client-id=${clientId}&currency=${currencyCode}&disable-funding=card,credit`
+    var script = document.createElement('script')
+    script.setAttribute('src', sdkUrl)
+    document.head.appendChild(script)
+  }
+}
