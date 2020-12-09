bb7403ad682a33941b451d247c5d661db4fa28f8
Syed Ali Meesum <alimeesum2056@gmail.com>
2020-12-09 15:29:24 +0500

    g-tag code added for body

--- src/themes/bedandfactory/App.vue
+++ src/themes/bedandfactory/App.vue
@@ -1,5 +1,15 @@
 <template>
   <div id="app">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript
+      ><iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-TCDSZMQ"
+        height="0"
+        width="0"
+        style="display: none; visibility: hidden"
+      ></iframe
+    ></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <component :is="layout">
       <router-view />
     </component>
@@ -7,23 +17,26 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
-const DefaultLayout = () => import(/* webpackChunkName: "vsf-layout-default" */ './layouts/Default')
-const EmptyLayout = () => import(/* webpackChunkName: "vsf-layout-empty" */ './layouts/Empty')
-const MinimalLayout = () => import(/* webpackChunkName: "vsf-layout-minimal" */ './layouts/Minimal')
+import { mapState } from 'vuex';
+const DefaultLayout = () =>
+  import(/* webpackChunkName: "vsf-layout-default" */ './layouts/Default');
+const EmptyLayout = () =>
+  import(/* webpackChunkName: "vsf-layout-empty" */ './layouts/Empty');
+const MinimalLayout = () =>
+  import(/* webpackChunkName: "vsf-layout-minimal" */ './layouts/Minimal');
 
 export default {
-  data () {
+  data() {
     return {
       ordersData: []
-    }
+    };
   },
   computed: {
     ...mapState({
       overlayActive: state => state.ui.overlay
     }),
-    layout () {
-      return `${(this.$route.meta.layout || 'default')}-layout`
+    layout() {
+      return `${this.$route.meta.layout || 'default'}-layout`;
     }
   },
   components: {
@@ -31,5 +44,5 @@ export default {
     EmptyLayout,
     MinimalLayout
   }
-}
+};
 </script>
