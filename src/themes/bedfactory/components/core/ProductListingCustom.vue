699f53b744b0f570360a128ecb29ff2fc75be46f
Syed Ali Meesum <alimeesum2056@gmail.com>
2021-04-16 12:25:29 +0500

    145 All code merged

--- src/themes/bedfactory/components/core/ProductListingCustom.vue
+++ src/themes/bedfactory/components/core/ProductListingCustom.vue
@@ -31,7 +31,8 @@ import ProductTileCustom from './ProductTileSearch.vue'
 export default {
   name: 'ProductListing',
   components: {
-    ProductTileCustom
+    ProductTileCustom,
+    ProductTileCustomCat
   },
   data () {
     return {
