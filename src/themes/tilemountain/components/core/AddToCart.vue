f56d9fb4ffae0b948bb9255923c97ee5276429d5
Syed Ali Meesum <alimeesum2056@gmail.com>
2021-06-09 12:45:17 +0500

    VTU-22 #cart page updated

--- src/themes/tilemountain/components/core/AddToCart.vue
+++ src/themes/tilemountain/components/core/AddToCart.vue
@@ -1,16 +1,25 @@
 <template>
-  <button-full
-    @click.native="
-      addToCart(product);
-      cartQuantityExceeded();
-      outOfStock(true);
-      closeModal();
-    "
-    :disabled="isProductDisabled"
-    data-testid="addToCart"
-  >
-    {{ $t('Add to basket') }}
-  </button-full>
+  <div class="addtocart-popup">
+    <button-full
+      @click.native="
+        addToCart(product);
+        showPopUp(product);
+      "
+      :disabled="!isProductDisabled"
+      data-testid="addToCart"
+    >
+      {{ $t("Add to cart") }}
+    </button-full>
+    <popup-cart
+      :productOptions="productOptions"
+      class="popup-main-cart"
+      :product="product"
+      :custom-options="customOptions"
+      v-if="cartpopupshow"
+      @popInterface="handlePopupAfteBack"
+      :productCalculatedPrice="productCalculatedPrice"
+    />
+  </div>
 </template>
 
 <script>
@@ -18,185 +27,261 @@ import { formatProductMessages } from '@vue-storefront/core/filters/product-mess
 import { notifications } from '@vue-storefront/core/modules/cart/helpers'
 import focusClean from 'theme/components/theme/directives/focusClean'
 import ButtonFull from 'theme/components/theme/ButtonFull.vue'
-import config from 'config'
-import axios from 'axios'
 import { mapGetters } from 'vuex'
-import { MeasureProductClick } from 'src/modules/google-gtag/mixins/MeasureProductClick'
-import { getProductPrimaryCategory, isServer } from '@vue-storefront/core/helpers'
+import PopupCart from "theme/components/PopupCart";
 
 export default {
   directives: { focusClean },
-  mixins: [MeasureProductClick],
-  components: { ButtonFull },
+  components: { ButtonFull , PopupCart },
   props: {
+    productCalculatedPrice: {
+      required: true,
+      type: Object,
+    },
+    productOptions: {
+      required: true,
+      type: Array,
+    },
     product: {
       required: true,
-      type: Object
+      type: Object,
+    },
+    customOptions: {
+      type: Object,
+      default: () => ({}),
     },
     disabled: {
       type: Boolean,
-      default: false
+      default: false,
     },
-    sqmValUpdated: {
-      type: [String, Number],
-      required: false,
-      default: null
-    }
-  },
-  data () {
-    return {
-      cartItems: 0
+    disableProduct: {
+      type: Boolean,
+      default: true
+    },
+    disableProductFlag: {
+      type: Boolean,
+      default: false
     }
   },
   methods: {
-    closeModal () {
-      this.$bus.$emit('modal-hide', 'modal-calculator')
+    showPopUp(product) {
+      this.cartpopupshow = true;
+      // var x = document.getElementsByTagName("BODY")[0];
+      // x.style.overflow ="hidden";
+      this.$bus.$emit("modal-show", "modal-switcher");
     },
-    async pullCartSync () {
-      await this.$store.dispatch('cart/sync', {
-        forceClientState: false,
-        forceSync: true
-      });
-      this.$forceUpdate();
-    },
-    notifyUser (notificationData) {
-      // this.$store.dispatch('notification/spawnNotification', notificationData, {
-      //   root: true
-      // });
+    handlePopupAfteBack(event) {
+      console.log("EventEventEvent ", event);
+      this.cartpopupshow = event;
+      var x = document.getElementsByTagName("BODY")[0];
+      x.style.overflow ="scroll";
     },
     onAfterRemovedVariant () {
       this.$forceUpdate()
     },
-    getProductOrCategoryPageMetaInfo (obj = this) {
-      // console.log('selectedObj', obj?.$options?.name);
-      if (obj && (!obj.$options || !obj.$options.name || obj.$options.name !== 'DefaultLayout') && obj.$parent) {
-        return this.getProductOrCategoryPageMetaInfo(obj.$parent);
-      }
-      if (obj?.$options?.name === 'DefaultLayout') {
-        let component = obj.$children.find(c => ['ProductPage', 'CategoryPage', 'HomePage'].includes(c?.$options?.name))
-        return { name: component?.$options?.name, meta: { ...component.$metaInfo } }
-      }
-    },
-    cartQuantityExceeded () {
-      if ((this.cartItems > this.product.stock.qty) || (this.sqmValUpdated > (this.product.stock.qty / this.product.qty_per_sqm))) {
-        if (this.product.clearance === 1) {
-          this.$bus.$emit('modal-show', 'modal-clearancemodal')
-          this.$bus.$emit('sendvalueToModal', this.cartItems)
-          return true
-        }
-      }
-      return false
-    },
-    outOfStock (fromTemp) {
-      let getAllStockStatus = config.customStockStatus
-      if (getAllStockStatus) {
-        let convertStockCodeToArray = Object.keys(getAllStockStatus).map((key) => getAllStockStatus[key])
-        const comapreCustomStockCode = this.product.custom_stock_status
-        if (convertStockCodeToArray.includes(String(comapreCustomStockCode))) {
-          if (
-            ((this.cartItems > this.product.stock.qty) ||
-            (this.sqmValUpdated > (this.product.stock.qty / this.product.qty_per_sqm))) && !this.cartQuantityExceeded()
-          ) {
-            let data = {
-              sku: this.product.sku,
-              name: this.product.name,
-              stock: this.product.stock.qty,
-              order: this.product.qty
-            }
-            let url = config?.sendoutstockEmail?.sendEmail || config?.api?.url + '/api/ext/outofstockEmail/sendEmail'
-            if (fromTemp) {
-              axios.post(url, data).then(({ data }) => {
-              })
-            }
-            this.$bus.$emit('modal-show', 'outofstock-modal')
-            return true
-          }
-        }
-        return false
-      }
-    },
     async addToCart (product) {
-      this.$emit('atcPixel')
-      if (!this.cartQuantityExceeded() && !this.outOfStock(false)) {
-        this.$emit('Modal')
-        if (this.sqmValUpdated === '') {
-          let tileQty = 0
-          let sqmQty = 1
-          tileQty = Math.ceil(product.qty_per_sqm + sqmQty)
-          product.qty = tileQty
-          this.$emit('update-sqm-qty', {
-            sqm: 1,
-            tileQty: tileQty
-          })
-        }
-        try {
-          const diffLog = await this.$store.dispatch('cart/addItem', {
-            productToAdd: product
-          })
-
-          this.$bus.$emit('cartUpdated', 'Product')
-          diffLog.clientNotifications.forEach((notificationData) => {
-            this.notifyUser(notificationData)
-          })
-
-          await this.pullCartSync();
-          let cartProduct = null
-          if (product.type_id === 'simple') {
-            cartProduct = this.currentCartItems?.find(c => c.sku === product.sku)
-          }
-
-
-          let page = this.getProductOrCategoryPageMetaInfo();
-          let productCat = getProductPrimaryCategory(product);
-
-          await this.sendAddCartProdClick({
-            product: cartProduct || product,
-            page,
-            priceFromTotals: false,
-            category: productCat
-          })
-
-          // this.$bus.$emit('new-item-added-to-cart', { diffLog, product });
-          this.$bus.$emit('product-added-to-cart')
-        } catch (message) {
-          console.log('Try Method Was failed', message);
-          // this.notifyUser(notifications.createNotification({ type: 'error', message }))
-        }
-      } else {
-        let page = this.getProductOrCategoryPageMetaInfo();
-        let productCat = getProductPrimaryCategory(product)
-        await this.sendAddCartProdClick({
-          product: product,
-          page,
-          priceFromTotals: false,
-          category: productCat
+      try {
+        const diffLog = await this.$store.dispatch('cart/addItem', { productToAdd: product })
+        diffLog.clientNotifications.forEach(notificationData => {
+          this.notifyUser(notificationData)
         })
+      } catch (message) {
+        this.notifyUser(notifications.createNotification({ type: 'error', message }))
       }
+    },
+    notifyUser (notificationData) {
+      this.$store.dispatch('notification/spawnNotification', notificationData, { root: true })
     }
   },
   computed: {
     ...mapGetters({
-      isAddingToCart: 'cart/getIsAdding',
-      currentCartItems: 'cart/getCartItems'
+      isAddingToCart: 'cart/getIsAdding'
     }),
-    isProductDisabled () {
+    isProductDisabled() {
+      console.log("112233445577 Answer is ",this.disableProduct,this.disableProductFlag,this.customOptions && this.customOptions.length > 0,this.customOptions , this.customOptions.length > 0,this.cOptionCheck , this.ccOptionCheck);
+       if (this.disableProductFlag) {
+return this.disableProduct;
+       }
+       else{
+         
+         //      console.log("112233445577 Answer is ",this.customOptionsCheck,this.customOptionsChecking);
+    //  console.log("112233445577 product options ",this.productOptions,"\n\nCustomOptions\n",this.customOptions,"\n\n\n",this.disabled,"\n\n\n",this.customOptionsCheck,"\n\n\n");
+    console.log("1122334455 ",formatProductMessages(this.product.errors),this.product.errors,"\n is adding to cart",this.isAddingToCart,"\n\n",this.disabled,"\n\n",(
+        this.disabled ||
+        this.cFlag ||
+        this.isAddingToCart
+      ));
+      console.log("1122334455 product is ",this.product);
       return (
         this.disabled ||
-        formatProductMessages(this.product.errors) !== '' ||
+        this.cFlag ||
         this.isAddingToCart
-      )
+      );
+       }
+    },
+    cOptionCheck(){
+      let cOPtions= this.customOptions;
+      console.log("112233-----> ",cOPtions);
+      this.isProductDisabledcList = []
+      this.cList=[]
+      this.cFlag = true;
+      for (let opt in this.customOptions){
+          console.log("Option is ",opt);
+        console.log(this.customOptions[opt].option_id);
+        console.log(this.customOptions[opt].option_value , );
+        // flag true
+            // if i === null
+            // flag false
+        if (this.customOptions[opt].option_value !== null){
+        let  newFlag ={
+          option_id: this.customOptions[opt].option_id,
+          value : true
+          }
+        this.cList.push(newFlag)
+         // this.cFlag = true
+        }
+        else{
+           let  newFlag ={
+             option_id: this.customOptions[opt].option_id,
+             value : false
+                  }
+        this.cList.push(newFlag)
+         // this.cFlag = false
+        }
+        console.log("this.customOptions",this.customOptions);
+        
+      }
+    },
+    ccOptionCheck(){
+      // this.cList.forEach((option,index)=>{
+      //   console.log("ssssssss ",option);
+      // })
+      this.cFlag=true
+      for (let opt in this.cList){
+        console.log("2255 ",this.cList[opt]);
+        if(this.cList[opt].value !== true){
+          this.cFlag =false;
+        }
+
+      }
+
+
+    },
+    customOptionsCheck(){
+     // console.log("112233445577 custom options is ",typeof this.customOptions ,this.customOptions , this.customOptions.length ,this.customOptions && this.customOptions.length>0);
+      // if(this.customOptions && this.customOptions.length>0){this.customOptions.forEach((option,index)=>{
+      //   console.log("112233445577 options is ",option);
+      // });
+      // }
+      this.optionsFlag =[];
+      if (this.product.custom_options) {
+        console.log("112233445577 product has custom options");
+        if (this.product.custom_options.length > 0) {
+          console.log("112233445577 length of custom options : ",this.product.custom_options.length);
+         this.product.custom_options.forEach((option, index) => {
+            // this.customOptions[
+            //       option.option_id
+            //     ].option_value
+            console.log("112233445577 in loop First option is ",index,option.option_id, this.customOptions[option.option_id]);
+            if(this.customOptions[option.option_id]){
+             console.log("112233445577",this.customOptions[option.option_id].option_value ,this.customOptions[option.option_id].option_value, this.customOptions[option.option_id].option_value && this.customOptions[option.option_id].option_value==null);
+              if(this.customOptions[option.option_id].option_value && this.customOptions[option.option_id].option_value!==null){
+              let newFlag ={
+                    option_id: option.option_id,
+                    value : true
+                  }
+                  this.optionsFlag.push(newFlag)
+              //  console.log("112233445577 options is ",option.option_id , this.customOptions["13"],this.customOptions[option.option_id],this.customOptions[option.option_id].option_value);
+                console.log("112233445577 options is ",this.product.custom_options.length ,index);
+                if (this.product.custom_options.length == index+1) {
+
+                  console.log("112233445577 All options complete returning true",this.customOptions);
+                  this.cusOptionChecked =true;
+                  // return true
+                  this.optionsFlag.forEach((cOption,cIndex)=>{
+                    console.log("112233445577 NEW option is ",option);
+                    if(option.value !== true){
+                      console.log("Value is not true",option.value);
+                        this.customOptionFlag = false
+                      return false
+                      }
+                    else{
+                        console.log("Value is true",option.value);
+                        }
+                        if (this.optionsFlag.length == cIndex+1) {
+                          if (this.customOptionFlag !== true){
+                            this.customOptionFlag = false
+                          }
+                          else{
+                            this.customOptionFlag =true
+                          }
+                        }
+      });
+                }
+                }
+                else {
+                  console.log("112233445577 check failed 1 ");
+                  this.flag =false
+                 let newFlag ={
+                    option_id: option.option_id,
+                    value : false
+                  }
+                  this.optionsFlag.push(newFlag)
+                  this.cusOptionChecked =false;
+                  return "Check Failed"
+                }
+                }
+                else {
+                  console.log("112233445577 check failed 2");
+                   let newFlag ={
+                    option_id: option.option_id,
+                    value : false
+                  }
+                  this.optionsFlag.push(newFlag)
+                  this.flag =false
+                  this.cusOptionChecked =false;
+                  return "Check Failed 2"
+                }
+           });
+        } else {
+          return {};
+        }
+      } else {
+        return {};
+      }
+    },
+    customOptionsChecking(){
+      this.optionsFlag.forEach((option,index)=>{
+        console.log("112233445577 NEW option is ",option);
+        if(option.value != true){
+          console.log("Value is not true",option.value);
+        //  this.customOptionFlag = false
+          return false
+        }
+        else{
+          console.log("Value is true",option.value);
+        }
+      });
     }
   },
-  mounted () {
-    this.$bus.$on('sendvalueToCart', (value) => {
-      this.cartItems = value
-    })
-  },
   beforeMount () {
+    console.log('SomethingToTest', this.productOptions, this.customOptions, this.productCalculatedPrice)
     this.$bus.$on('product-after-removevariant', this.onAfterRemovedVariant)
   },
   beforeDestroy () {
     this.$bus.$off('product-after-removevariant')
   }
-};
+}
 </script>
+<style scoped>
+.bt-product-addtocartbtn .addtocart-popup button {
+  padding: 7.5px 15px;
+  border-bottom-left-radius: 6px;
+  border-top-left-radius: 6px;
+  border-top-right-radius: 6px;
+  border-bottom-right-radius: 6px;
+  background: #4dba87;
+  text-transform: uppercase;
+  font-weight: bold;
+}
+</style>
