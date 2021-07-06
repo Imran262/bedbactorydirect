b282853a1a952b00d17609d82151c5c314badc29
Syed Ali Meesum <alimeesum2056@gmail.com>
2021-07-06 14:42:33 +0500

    #VTU-61 VTU-57 alt tags added to brands on home page

--- src/themes/bedfactory/components/theme/blocks/Brands/Branddetail.vue
+++ src/themes/bedfactory/components/theme/blocks/Brands/Branddetail.vue
@@ -8,7 +8,7 @@
       <ul>
         <li>
           <a href>
-            <img :src="singleblock.img" />
+            <img :src="singleblock.img" :alt="singleblock.alt"/>
           </a>
         </li>
       </ul>
@@ -25,13 +25,14 @@ export default {
       var homeSmallBlocks = [];
       if (this.data) {
         var mbclasses = this.parsedContent.querySelectorAll(".mb_home_img");
-        // console.log('Sb -class ', mbclasses);
+        console.log('Sb -class ', mbclasses);
         var i = 0;
         [].forEach.call(mbclasses, mbclass => {
           var singleBlock = new Array(3);
 
-          singleBlock["atag"] = mbclass.querySelector("a").attributes["href"];
-          singleBlock["img"] = mbclass.querySelector("img").attributes["src"];
+          singleBlock["atag"] = mbclass.querySelector("a")? mbclass.querySelector("a").attributes["href"] ? mbclass.querySelector("a").attributes["href"] :' ' :' ';
+          singleBlock["img"] = mbclass.querySelector("img")? mbclass.querySelector("img").attributes["src"] ? mbclass.querySelector("img").attributes["src"] :' ' :' ';
+          singleBlock["alt"] = mbclass.querySelector("img")? mbclass.querySelector("img").attributes["alt"] ? mbclass.querySelector("img").attributes["alt"] :' ' :' ';
           homeSmallBlocks[i] = singleBlock;
           i++;
         });
