65c7e64385161cfc176e47777d89f67b287cbad0
aamir shahzad <aamir.shahzad@ki5.co.uk>
2021-08-10 14:41:02 +0100

    Merge conflicts resolved with VTU-101

--- src/themes/tilemountain/components/core/blocks/SidebarMenu/SubBtn.vue
+++ src/themes/tilemountain/components/core/blocks/SidebarMenu/SubBtn.vue
@@ -106,7 +106,7 @@ export default {
         let bodyVar = document.getElementsByTagName('BODY')[0]
         let sidebarMenu = document.getElementById('sidebarmenucontainer')
         if (sidebarMenu != null) {
-               console.log('vissible')
+          console.log('9512 vissible sub btn')
           return bodyVar.classList.toggle('body-fixed')
         }
       }, 100)
@@ -347,6 +347,6 @@ button {
   padding-left: 0;
 }
 .body-fixed {
-  position: fixed;
+  /* position: fixed; */
 }
 </style>
