896bac6a0fb2d96d8147d08cc3aafa19077a7712
Syed Ali Meesum <alimeesum2056@gmail.com>
2021-09-02 12:59:40 +0500

    Bread crumbs issue fixed | category banner tem image added

--- src/themes/bedfactory/components/core/Breadcrumbs.vue
+++ src/themes/bedfactory/components/core/Breadcrumbs.vue
@@ -1,25 +1,55 @@
 <template>
-  <div class="breadcrumbs h5 cl-gray" itemscope itemtype="https://schema.org/BreadcrumbList">
-  <router-link :to="'/'" itemprop="item">
-         <span class="no-padding" itemprop="name"> Home </span>
-          
-        </router-link>
-    <span v-for="(link, index) in paths" :key="link.route_link" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+  <div
+    class="breadcrumbs h5 cl-gray"
+    itemscope
+    itemtype="https://schema.org/BreadcrumbList"
+  >
+    <span
+      itemprop="itemListElement"
+      itemscope
+      itemtype="https://schema.org/ListItem"
+    >
+      <router-link :to="'/'" itemprop="item">
+        <span class="no-padding" itemprop="name"> Home </span>
+      </router-link>
+    </span>
+    <span
+      v-for="(link, index) in paths"
+      :key="link.route_link"
+      itemprop="itemListElement"
+      itemscope
+      itemtype="https://schema.org/ListItem"
+    >
       <template
         v-if="link.name !== 'Default Category' && link.name !== 'New Default'"
       >
-        <router-link :to="link.name.includes('Root') ? '/' : link.route_link" itemprop="item" :class="paths.length - 1 == index ? 'LastElement' : ''">
-          <template v-if="link.name.includes('Root')"> <span class="no-padding" itemprop="name"> Home </span></template>
+        <router-link
+          :to="link.name.includes('Root') ? '/' : link.route_link"
+          itemprop="item"
+          :class="paths.length - 1 == index ? 'LastElement' : ''"
+        >
+          <template v-if="link.name.includes('Root')">
+            <span class="no-padding" itemprop="name"> Home </span></template
+          >
           <template v-else>
-            <span class="no-padding" itemprop="name"> {{ link.name | htmlDecode }} </span>
+            <span class="no-padding" itemprop="name">
+              {{ link.name | htmlDecode }}
+            </span>
           </template>
         </router-link>
         <meta itemprop="position" :content="index + 1" />
       </template>
       /
     </span>
-    <span class="cl-mine-shaft" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
-      <span class="no-padding" itemprop="name" id="current-tag-name">{{ current | htmlDecode }}</span>
+    <span
+      class="cl-mine-shaft"
+      itemprop="itemListElement"
+      itemscope
+      itemtype="https://schema.org/ListItem"
+    >
+      <span class="no-padding" itemprop="name" id="current-tag-name">{{
+        current | htmlDecode
+      }}</span>
       <meta itemprop="position" :content="paths.length + 1" />
     </span>
   </div>
@@ -44,7 +74,7 @@ export default {
     // color: #b2b2b1;
     font-size: 12px;
     font-family: Arial;
-    :hover{
+    :hover {
       color: #ca0413;
     }
   }
