d4539b1a0812cf23240dc679851b9bb43222096d
Aliraza-5 <ali.raza@ki5.co.uk>
2021-09-21 15:11:43 +0500

    financesuccess page added

---
+++ src/themes/bedfactory/pages/Financesuccess.vue
@@ -0,0 +1,27 @@
+<template>
+    <button type="button" data-testid="addToCart" class="financesuccess">
+   Apply For Finance
+  </button>
+</template>
+
+<style scoped>
+.financesuccess{
+    border-radius: 3px;
+    color: #fff;
+    cursor: pointer;
+    display: block;
+    font-size: 14px;
+    max-width: 260px;
+    min-height: auto;
+    position: relative;
+    text-align: center;
+    width: 100%;
+    font-family: Arial;
+    font-weight: bold;
+    padding: 15px 0px;
+    text-transform: uppercase;
+    margin-top: 5px;
+    float: right;
+    background: #071a44;
+}
+</style>
