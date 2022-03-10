fd715d3cd28ad5ef85ad9f3d231e3d0bc0929ac1
Syed Ali Meesum <alimeesum2056@gmail.com>
2022-03-10 18:06:50 +0500

    VTU-179 # Products not loading issue fixed

--- src/themes/bedfactory/pages/Category.vue
+++ src/themes/bedfactory/pages/Category.vue
@@ -669,7 +669,7 @@ export default {
     setTimeout(() => {
       console.log("7456321 After 5 seconds",this.originalRoute);
       this.$router.push(this.localizedRoute(this.originalRoute));
-    }, 5000);
+    }, 100);
       }
     
     await this.getBrandData();
